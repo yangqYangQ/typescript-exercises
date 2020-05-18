efa8fed57e4e79d07cc5fbad7d80587c89050b34
yangq <yangq0979@gmail.com>
2020-05-18 16:07:05 +0800

    05 重载

--- exercises/exercise-05/index.ts
+++ exercises/exercise-05/index.ts
@@ -50,12 +50,12 @@ interface Admin {
 type Person = User | Admin;
 
 const persons: Person[] = [
-    { type: 'user', name: 'Max Mustermann', age: 25, occupation: 'Chimney sweep' },
-    { type: 'admin', name: 'Jane Doe', age: 32, role: 'Administrator' },
-    { type: 'user', name: 'Kate Müller', age: 23, occupation: 'Astronaut' },
-    { type: 'admin', name: 'Bruce Willis', age: 64, role: 'World saver' },
-    { type: 'user', name: 'Wilson', age: 23, occupation: 'Ball' },
-    { type: 'admin', name: 'Agent Smith', age: 23, role: 'Anti-virus engineer' }
+    {type: 'user', name: 'Max Mustermann', age: 25, occupation: 'Chimney sweep'},
+    {type: 'admin', name: 'Jane Doe', age: 32, role: 'Administrator'},
+    {type: 'user', name: 'Kate Müller', age: 23, occupation: 'Astronaut'},
+    {type: 'admin', name: 'Bruce Willis', age: 64, role: 'World saver'},
+    {type: 'user', name: 'Wilson', age: 23, occupation: 'Ball'},
+    {type: 'admin', name: 'Agent Smith', age: 23, role: 'Anti-virus engineer'}
 ];
 
 function logPerson(person: Person) {
@@ -64,19 +64,27 @@ function logPerson(person: Person) {
     );
 }
 
-function filterPersons(persons: Person[], personType: string, criteria: unknown): unknown[] {
+type FilterCriteria<T> = Partial<Omit<T, 'type'>>;
+
+function getObjectKeys<T>(obj: T): (keyof T)[] {
+    return Object.keys(obj) as (keyof T)[];
+}
+
+function filterPersons(persons: Person[], personType: 'user', criteria: FilterCriteria<User>): User[];
+function filterPersons(persons: Person[], personType: 'admin', criteria: FilterCriteria<Admin>): Admin[];
+function filterPersons(persons: Person[], personType: 'user' | 'admin', criteria: FilterCriteria<Person>): Person[] {
     return persons
         .filter((person) => person.type === personType)
         .filter((person) => {
-            let criteriaKeys = Object.keys(criteria) as (keyof Person)[];
+            let criteriaKeys = getObjectKeys(criteria);
             return criteriaKeys.every((fieldName) => {
                 return person[fieldName] === criteria[fieldName];
             });
         });
 }
 
-let usersOfAge23: User[] = filterPersons(persons, 'user', { age: 23 });
-let adminsOfAge23: Admin[] = filterPersons(persons, 'admin', { age: 23 });
+let usersOfAge23: User[] = filterPersons(persons, 'user', {occupation: 'Ball'});
+let adminsOfAge23: Admin[] = filterPersons(persons, 'admin', {age: 23});
 
 console.log(chalk.yellow('Users of age 23:'));
 usersOfAge23.forEach(logPerson);
