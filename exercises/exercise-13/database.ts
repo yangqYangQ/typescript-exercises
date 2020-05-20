b1aef0af325b1ee51d2dfedf112936879d87343a
yangq <yangq0979@gmail.com>
2020-05-20 15:45:57 +0800

    13 只补充了类型，没有写 find 查询方法的具体实现 太难啦

--- exercises/exercise-13/database.ts
+++ exercises/exercise-13/database.ts
@@ -1,13 +1,21 @@
 export class Database<T> {
     protected filename: string;
-    protected fullTextSearchFieldNames: unknown[];
+    protected fullTextSearchFieldNames: Array<keyof T>;
 
-    constructor(filename: string, fullTextSearchFieldNames) {
+    constructor(filename: string, fullTextSearchFieldNames: Array<keyof T>) {
         this.filename = filename;
         this.fullTextSearchFieldNames = fullTextSearchFieldNames;
     }
 
-    async find(query): Promise<T[]> {
+    async find(query: Query<T>): Promise<T[]> {
         return [];
     }
 }
+
+type Query<T> = { [K in keyof T]?: FieldQuery<T[K]> } & {
+    $and?: Query<T>[];
+    $or?: Query<T>[];
+    $text?: string;
+}
+
+type FieldQuery<FT> = { $eq: FT } | { $gt: FT } | { $lt: FT } | { $in: FT[] };
