5c905c50c45212348ee34b829541472cdec36091
yangq <yangq0979@gmail.com>
2020-05-20 14:35:24 +0800

    12 declare module 扩展原有模块的类型

--- exercises/exercise-12/module-augmentations/date-wizard/index.ts
+++ exercises/exercise-12/module-augmentations/date-wizard/index.ts
@@ -3,4 +3,11 @@ import 'date-wizard';
 
 declare module 'date-wizard' {
     // Add your module extensions here.
+    export function pad(s: number): string;
+
+    interface DateDetails {
+        hours: number;
+        minutes: number;
+        seconds: number;
+    }
 }
